dd48a38a7e7115b6584a07b34948573244ae8f67
pawel <pawel.morawiak@studenci.collegiumwitelona.pl>
2025-05-30 02:15:04 +0200

    header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,13 +4,12 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Posts</title>
+  <title>Main</title>
 </head>
 <body>
 
-
-
-
+do sprawdzenia 
+ 'auth()->login();' zmienic na 'auth()->guard()->login();'
 <?--// uzytkownik zalogowany, wyswietlany ponizszy panel --?>
 
 @auth 
